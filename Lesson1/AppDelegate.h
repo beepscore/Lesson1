d58b45c520c96f7ea857aa919a262e2d9b934610
beepscore <support@beepscore.com>
2011-11-21 22:20:57 -0800

    Change name from __MyCompanyName__ to Beepscore LLC

--- Lesson1/AppDelegate.h
+++ Lesson1/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Lesson1
 //
 //  Created by Steve Baker on 11/21/11.
-//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//  Copyright Beepscore LLC 2011. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
